c65fcb1f54019b5b13bbda26c1608622db57df0c
Douglas Gregor <dgregor@cs.indiana.edu>
2005-11-15 14:41:53 +0000

    A bunch of new code to mak beautiful documentation for... just BFS, at the moment.

--- python/boost/graph/__init__.py
+++ python/boost/graph/__init__.py
@@ -12,3 +12,27 @@ from bfs_visitor import bfs_visitor
 from bellman_ford_visitor import bellman_ford_visitor
 from dfs_visitor import dfs_visitor
 from dijkstra_visitor import dijkstra_visitor
+
+# Introduce documentation for all of the algorithms in the module.
+# Doing so here is *much* easier than doing it in the C++ code,
+# because we don't need to recompile to change documentation and we
+# have the option to do fancier things within the Python code to make
+# our lives easier.
+import docstring
+documentor = docstring.docstring()
+
+# The actual documentation
+breadth_first_search.__doc__ = documentor.function('breadth_first_search') \
+    .parameter('graph', 
+               'the graph on which the breadth-first search will run. It may be either a directed or undirected graph.') \
+    .parameter('root_vertex', 'the vertex where the breadth-first search will originate.') \
+    .parameter('buffer', 'the queue used by the breadth-first search. If not supplied, a simple FIFO queue will be used.', 'None') \
+    .parameter('visitor', 'a visitor that will receive events as the breadth-first search progresses. Typically this visitor should be derived from boost.graph.bfs_visitor.', 'None') \
+    .parameter('color_map', 'a vertex property map that stores the "color" of each vertex, which indicates whether is has not been seen (white), has been seen but not visited (grey), or has been visited (black).', 
+               'graph.vertex_property_map(\'color\')') \
+    .paragraph('Performs a breadth-first search on the given graph starting at a particular vertex.') \
+    .see_also('bfs_visitor') \
+    .see_also('breadth_first_visit') \
+    .see_also('depth_first_search') \
+    .__str__()
+  
---
+++ python/boost/graph/docstring.py
@@ -0,0 +1,203 @@
+# Copyright 2005 The Trustees of Indiana University.
+
+# Use, modification and distribution is subject to the Boost Software
+# License, Version 1.0. (See accompanying file LICENSE_1_0.txt or copy at
+# http:#www.boost.org/LICENSE_1_0.txt)
+
+#  Authors: Douglas Gregor
+#           Andrew Lumsdaine
+
+columns = 72
+
+def wrap(text, width):
+    """
+    A word-wrap function that preserves existing line breaks
+    and most spaces in the text. Expects that existing line
+    breaks are posix newlines (\n).
+    From: http://aspn.activestate.com/ASPN/Cookbook/Python/Recipe/148061
+    """
+    return reduce(lambda line, word, width=width: '%s%s%s' %
+                  (line,
+                   ' \n'[(len(line)-line.rfind('\n')-1
+                         + len(word.split('\n',1)[0]
+                              ) >= width)],
+                   word),
+                  text.split(' ')
+                 )
+
+class function_documentor:
+    def __init__(self, name):
+        self._name = name
+        self._parameters = list()
+        self._result = None
+        self._paragraphs = list()
+        self._example = None
+        self._see_also = list()
+
+    def parameter(self, name, doc, default = None):
+        """ 
+        Declares a function parameter given its name, a documentation
+        string, and (optionally) a string containing its default
+        value.
+        """
+        self._parameters.append((name, doc.strip(), default))
+        return self
+
+    def result(self, type):
+        """
+        Declares the type that will be returned by the function.
+        """
+        self._result = type
+        return self
+
+    def paragraph(self, para):
+        """
+        Add a paragraph of descriptive text.
+        """
+        self._paragraphs.append(para.strip())
+        return self
+
+    def example(self, example):
+        """
+        Add example code, written in Python of course.
+        """
+        self._example = example
+        return self
+
+    def see_also(self, other):
+        """
+        Add the name of something else that the might want to refer
+        to.
+        """
+        self._see_also.append(other)
+        return self
+
+    def __signature_string(self):
+        """
+        Builds a string containing the signature of this function, e.g.,
+
+          __signature_string(self) -> str
+        """
+        docstring = self._name + '('
+        if self._parameters != list():
+            param_start_column = len(self._name + '(')
+            column = param_start_column
+            max_column = columns - 2
+            for (on_param, param) in enumerate(self._parameters):
+                # Add ", " when needed
+                if on_param > 0:
+                    docstring += ', '
+                    column += 2
+
+                # If we're on the last parameter and there is a
+                # result, we have stricter requirements on maximum
+                # column.
+                if on_param+1 == len(self._parameters) and self._result != None:
+                    max_column = columns - len(') -> ') - len(self._result)
+
+                # Build a string for the parameter signature
+                paramstr = param[0]
+                if param[2] != None:
+                    paramstr += ' = ' + param[2]
+
+                # If the parameter won't fit on this list, and we're
+                # not already at the beginning of a new line, put in a
+                # newline.
+                if column + len(paramstr) > max_column and \
+                   column > param_start_column + 2:
+                    docstring += str('\n').ljust(param_start_column + 1)
+                    column = param_start_column
+                    
+                # Add the documentation for this parameter
+                docstring += paramstr
+                column += len(paramstr)
+        docstring += ')'
+
+        if self._result != None:
+            docstring += ' -> ' + self._result
+
+        return docstring
+
+    def __parameters_string(self):
+        """
+        Builds and returns a docstring containing documentation for
+        each of the parameters to the function.
+        """
+        if self._parameters == list():
+            return ''
+
+        docstring = """
+
+Parameters:
+"""
+    
+        # Compute maximum length of any parameter name
+        maxlen = 0
+        for param in self._parameters:
+            maxlen = max(maxlen, len(param[0]))
+
+        # Build documentation for parameters
+        for (on_param, param) in enumerate(self._parameters):
+            if on_param > 0:
+                docstring += '\n'
+
+            docstring += '  ' + param[0].ljust(maxlen + 2)
+            doc = wrap(param[1], columns - maxlen - 4)
+            padding = str('')
+            for line in doc.split('\n'):
+                docstring += padding + line + '\n'
+                padding = str('').ljust(maxlen + 4)
+                    
+        return docstring        
+
+    def __str__(self):
+        # Start with the signature
+        docstring = self.__signature_string()
+
+        # Add the documentation paragraphs
+        for para in self._paragraphs:
+            docstring += '\n\n'
+            docstring += wrap(para, columns)
+
+        # Add the documentation of each parameter
+        docstring += self.__parameters_string()
+                        
+        # Add the example
+        if self._example != None:
+            docstring += """
+
+Example:""" + self._example
+
+        # Add the "See also" section:
+        if self._see_also != list():
+            docstring += """
+
+See also:"""
+            for other in self._see_also:
+                docstring += '\n  ' 
+                docstring += other
+
+        # Add the "Complete C++ documentation" section
+        docstring += """
+
+Complete documentation:
+  http://www.boost.org/libs/graph/doc/""" + self._name + ".html"
+
+        return docstring
+
+                    
+                
+                
+
+
+class docstring:
+    """
+    Produces documentation in Python docstring format for the Boost
+    Graph Library's Python bindings using a simple textual format
+    suitable for display on simple terminals.
+    """
+
+    def function(self, name):
+        """Generates a function documentor object"""
+        return function_documentor(name)
+

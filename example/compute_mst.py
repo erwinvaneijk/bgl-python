6f8964fe19f3413456e523b9a8db2583511b3cd8
dgregor <dgregor@5701c2ee-6afb-0310-bf4e-950296de0365>
2005-11-21 22:04:11 +0000

    Tweak the README and add an example
    git-svn-id: https://svn.osl.iu.edu/svn/projects_viz/bgl-python@162 5701c2ee-6afb-0310-bf4e-950296de0365

---
+++ example/compute_mst.py
@@ -0,0 +1,40 @@
+# Copyright 2005 The Trustees of Indiana University.
+
+# Use, modification and distribution is subject to the Boost Software
+# License, Version 1.0. (See accompanying file LICENSE_1_0.txt or copy at
+# http:#www.boost.org/LICENSE_1_0.txt)
+
+#  Authors: Douglas Gregor
+#           Andrew Lumsdaine
+import boost.graph as bgl
+
+# Load a graph from the GraphViz file 'mst.dot'
+graph = bgl.Graph.read_graphviz('mst.dot')
+
+# Compute the minimum spanning tree of the graph
+in_weight = graph.edge_properties['weight']
+
+# Convert the weight into floating-point values
+weight = graph.edge_property_map('float')
+for e in graph.edges:
+    weight[e] = float(in_weight[e])
+
+mst_edges = bgl.kruskal_minimum_spanning_tree(graph, weight)
+
+# Put the weights into the label. Make MST edges solid while all other
+# edges remain dashed.
+label = graph.edge_property_map('string')
+style = graph.edge_property_map('string')
+for e in graph.edges:
+    label[e] = str(weight[e])
+    if e in mst_edges:
+        style[e] = 'solid'
+    else:
+        style[e] = 'dashed'
+
+# Associate the label and style property maps with the graph for output
+graph.edge_properties['label'] = label
+graph.edge_properties['style'] = style
+
+# Write out the graph in GraphViz DOT format
+graph.write_graphviz('mst-out.dot')

e07bebc2ce5b87ad1406bae8b931accd13663671
Douglas Gregor <dgregor@cs.indiana.edu>
2006-09-18 17:03:00 +0000

    Make the hide example make sense

--- example/hide.py
+++ example/hide.py
@@ -1,11 +1,11 @@
 import boost.graph as bgl
 
-g = bgl.Graph((('a', 'b'), ('b', 'c'), ('c', 'd'), ('d', 'a')))
+g = bgl.Graph((('a', 'b'), ('b', 'c'), ('c', 'd'), ('d', 'a')), 'name')
 
 print 'When not hidden, contains',len(bgl.biconnected_components(g, g.add_edge_property(type='integer'))),'articulation points'
 
 hide_edges = g.add_edge_property(type='integer')
-hide_edges[g.edges.next()] = 0
+hide_edges[g.edges.next()] = 1
 
 print 'When hidden, contains',len(bgl.biconnected_components(g, g.add_edge_property(type='integer'), hide_edges)),'articulation points'
 
